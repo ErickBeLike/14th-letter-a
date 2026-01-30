f64072e699fb4abaa26cc3b741bf3452f261b152
ErickBeLike <20223l001053@utcv.edu.mx>
2026-01-30 00:28:17 -0600

    Cosito 3

--- src/app/pages/gallery/gallery.component.ts
+++ src/app/pages/gallery/gallery.component.ts
@@ -14,56 +14,54 @@ export class GalleryComponent {
     {
       id: 1,
       title: 'Donde todo empezó',
-      date: 'Época Roblox',
+      date: 'Sale un Roblox?',
       images: [
-        'assets/images/roblox1.jpg', // Foto 1
-        'assets/images/roblox2.jpg', // Foto 2
-        'assets/images/roblox3.jpg', // Foto 3
+        'assets/images/roblox/roblox_1.jpg', // Foto 1
+        'assets/images/roblox/roblox_2.jpg', // Foto 2
+        'assets/images/roblox/roblox_3.png', // Foto 3
+        'assets/images/roblox/roblox_4.png', // Foto 4
+        'assets/images/roblox/roblox_5.png', // Foto 5
       ],
       description:
         'Aquí pondrás el texto cursi sobre cómo se conocieron en Roblox...',
     },
     {
       id: 2,
-      title: 'Mundo Cuadrado',
-      date: 'Época Minecraft',
+      title: 'Vamoh a jugar...',
+      date: 'Un poquito de to´',
       images: [
-        'assets/images/roblox1.jpg', // Foto 1
-        'assets/images/roblox2.jpg', // Foto 2
-        'assets/images/roblox3.jpg', // Foto 3
+        'assets/images/mc/mc_1.png',
+        'assets/images/peak/peak_1.png',
+
+        'assets/images/mc/mc_2.png',
+        'assets/images/peak/peak_2.png',
+
+        'assets/images/mc/mc_3.png',
       ],
       description: 'Las construcciones, las risas, el server...',
     },
     {
       id: 3,
-      title: 'El Prime',
-      date: 'Peak Gaming',
+      title: 'La presencia a distancia',
+      date: 'Dibujos que hablan',
       images: [
-        'assets/images/roblox1.jpg', // Foto 1
-        'assets/images/roblox2.jpg', // Foto 2
-        'assets/images/roblox3.jpg', // Foto 3
+        'assets/images/draw/draw_1.jpeg',
+        'assets/images/draw/draw_2.jpeg',
+        'assets/images/draw/draw_3.jpeg',
+        'assets/images/draw/draw_4.jpeg',
       ],
       description: 'Cuando jugaban todo el día...',
     },
     {
       id: 4,
-      title: 'Arte Compartido',
-      date: 'Nuestros Dibujos',
-      images: [
-        'assets/images/roblox1.jpg', // Foto 1
-        'assets/images/roblox2.jpg', // Foto 2
-        'assets/images/roblox3.jpg', // Foto 3
-      ],
-      description: 'El collage de los dibujos que se han hecho...',
-    },
-    {
-      id: 5,
       title: 'Distancia Cero',
-      date: 'El Balcón',
+      date: 'El encuentro',
       images: [
-        'assets/images/roblox1.jpg', // Foto 1
-        'assets/images/roblox2.jpg', // Foto 2
-        'assets/images/roblox3.jpg', // Foto 3
+        'assets/images/encounter/encounter_1.jpeg',
+        'assets/images/encounter/encounter_2.jpeg',
+        'assets/images/encounter/encounter_4.jpeg',
+        'assets/images/encounter/encounter_5.jpeg',
+        'assets/images/encounter/encounter_3.jpeg',
       ],
       description: 'Esa foto especial...',
     },
