73a7ef7a3df8d9a11aa3bb449c84dcff8a4e9f17
ErickBeLike <20223l001053@utcv.edu.mx>
2026-01-30 21:09:34 -0600

    Cosito 8

--- src/app/pages/gallery/gallery.component.ts
+++ src/app/pages/gallery/gallery.component.ts
@@ -29,7 +29,7 @@ export class GalleryComponent {
         'assets/images/roblox/roblox_5.png', // Foto 5
       ],
       description:
-        'Aquí pondrás el texto cursi sobre cómo se conocieron en Roblox...',
+        'Y de la nada un día muy casual jugando con Aza salió el tema de que tenía una amistad con alguien y que si me parecía que se jugara entre los 3, a lo que accedí sin pensarlo mucho. Realmente nunca pensé que de ahí evolucionaría a todo lo que vino después, pero me alegro mucho de haber dicho que sí. De ahí fue cuestión de tiempo para que la sesiones de Roblox se volviesen algo esperado día con día, semana con semana. Desde entonces no faltaron noches de terror, risas y aventuras en ese mundito robloxiano. He ahí parte del cariño y afecto que le tengo a nuestros personajes, fueron el principio de la representación de cada uno en ese espacio compartido. A todo esto, debería de salir un bobox de nuevo, no? :).',
     },
     {
       id: 2,
@@ -44,7 +44,7 @@ export class GalleryComponent {
 
         'assets/images/mc/mc_3.png',
       ],
-      description: 'Las construcciones, las risas, el server...',
+      description: 'El tiempo hizo de las suyas y eventualmente el Roblox se nos estaba quedando corto, mira que es raro por la cantidad de experiencias que tiene pero así fue. Fue ahí cuando salió la siguiente vida virtual y no podía ser otra que el Minecraft. Empezaron siendo sesiones cortas y casuales, pero eventualmente se volvieron algo más estructurado y planeado. Llegaron al punto de convertirse no solo en sesiones de juego sino hasta en sesiones de psicología mutua, o bueno, así lo sentía yo, jeje. Hasta la fecha tenemos muchos planes por hacer en ese mundito cúbico, y espero se puedan ir cumpliendo uno a uno. De ahí entró otro de nuestros pequeños "peaks", guiño, guiño, el cual no se queda corto en diversión y aventuras, literalmente, incluso se volvieron sesiones completas de infinidad de fotos. En una de esas que no haya muchas ocupaciones por parte de los dos estaría muy cuki volver a alguno de los 2, quizá en especial Minecraft, no? :D.',
     },
     {
       id: 3,
@@ -56,7 +56,7 @@ export class GalleryComponent {
         'assets/images/draw/draw_3.jpeg',
         'assets/images/draw/draw_4.jpeg',
       ],
-      description: 'Cuando jugaban todo el día...',
+      description: '¿Dibujos? querré decir obras de arte más bien, lo dicho, yo como persona soy alguien muy simbólico y cada que veía algún dibujito con el que me llegabas y decías "mira, dibujito" en ese momento, PUM, me alegraba por completo todo el día, semana o incluso mes. Probablemente haya quienes le encanten tus dibujos pero para mí tienen un significado mucho más profundo, representan tu presencia a distancia. Me autoproclamo el fan #1, tu fan #1, así de simple. Cada dibujo es como una pequeña ventana a tu mundo, tal cual como la vez que te lo comenté y por ende me alegra poder ver esa ventanita a ese tan bello mundo. Así que gracias por cada uno de esos dibujitos, significan mucho más de lo que imaginas. Lo he dicho, sin mis tesoros o como Gollum, son mis "preciosos". Como recién te conté, empecé a experimentar este mundito de dibujar por ti y contigo, he de decir que es hasta terapéutico y relajante, quizá algún día me animo a intentarlo en papel pero mientras, sigamos teniendo de manera espontánea esas sesiones de dibujo juntos, quizá en una de esas me pasas la "maña" para dibujar tan bien como tú ;).',
     },
     {
       id: 4,
@@ -69,7 +69,7 @@ export class GalleryComponent {
         'assets/images/encounter/encounter_5.jpeg',
         'assets/images/encounter/encounter_3.jpeg',
       ],
-      description: 'Esa foto especial...',
+      description: 'Y la guinda del pastel, el encuentro en sí. Mira que ya lo he contado pero siendo sinceros no me cansaría de hacerlo, todo empezó como pensamientos e ideas vagas en donde me decía "jolines tío, estaría flipante conocerla en persona", quizá sin el acento castellano, jeje, pero empezó ese pensamiento como una posibilidad un tanto casual. Pero literalmente a "poco" de haber tenido esa epifanía, se dio la oportunidad en forma de píldora roja o azul que me contaste, en donde al final por situaciones de la vida (y ayuda del guión) se dio paso al plan de visita-vacaciones. No voy a negar que los días previos a la confirmación fueron un poquito caóticos en mi mente, al menos en el sentido de que "santos cielos, van a venir, ¿qué haré?, ¿cómo me comportaré?, ¿qué tal si no nos llevamos bien en persona?" pero era súper raro porque lo dicho, iba a ser conocer a alguien que conoces de hace mucho pero que a su vez no conoces en NADA. Llegado el día, pese a los nervios iniciales todo fluyó de manera muy natural, con detallitos de por medio pero nada grave, lo seguiré diciendo, que el local de "esquites" estuviese abierto fue una ayuda divina XD. Los días posteriores fueron literalmente toda una mini aventura a lo Roblox, estuvimos en un castillo medieval, volvimos a la prehistoria, vimos dragones, estuvimos en la playita, tuvimos un imperio, perdimos un imperio (JAJAJAJ, nos faltó un tycoon) pero vaya, creo que puedo dar a entender que esos poquitos 3 días, fueron literalmente TODA una experiencia. Acerca del desenlace, he de decir que el destino me pasó el balón de pechito para que la despedida se sintiese bonito y heróico, desde unas florecitas así casuales para ti y tu mami hasta una salvada histórica (tenía que hacerme merecedor de mi armadura) así como lo dije en su entonces, final con broche de oro. En esos días hubo varias foticos de las cuales no sabes cómo me alegran el día y el corazón cada que las veo, son como nuestras screenshots de Roblox pero en 4K, de las cuales no está de más decir que la que se ganó el podio en mi mente y corazón fue la del balcón, como los dibujos, no quiero dar preferencia pero es que esa me hace unos ojitos que fua chaval. En fin, gracias tanto a ti como a tu mami por haber dado la oportunidad de que se diese esa experiencia, tocará ver cuándo y cómo se hace "El encuentro, VOL. 2". :D.',
     },
   ];
 
